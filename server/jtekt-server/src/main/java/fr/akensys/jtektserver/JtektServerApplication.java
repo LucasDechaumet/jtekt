52656f21a92a1d8b176a2b975caff491467b964e
Lucas <lucasdechaumet@outlook.com>
2024-05-13 16:47:24 +0200

    first commit

---
+++ server/jtekt-server/src/main/java/fr/akensys/jtektserver/JtektServerApplication.java
@@ -0,0 +1,13 @@
+package fr.akensys.jtektserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtektServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtektServerApplication.class, args);
+	}
+
+}
---
+++ server/jtekt-server/src/test/java/fr/akensys/jtektserver/JtektServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.akensys.jtektserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtektServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
